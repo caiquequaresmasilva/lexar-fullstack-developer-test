1cbb35730094083ddbd3c31607a800a3b8cb4fbe
caiquequaresmasilva <caique.quaresma@gmail.com>
2024-03-01 17:52:27 -0300

    feat(Infra): Implements JwtAdapter

--- backend/users-api/src/server.ts
+++ backend/users-api/src/server.ts
@@ -1,7 +1,9 @@
 import express from 'express';
+import 'dotenv/config'
 
 const app = express();
+const port = process.env.API_PORT
 
 app.get('/', (req, res) => res.json('Hello World!'));
 
-app.listen(3000, () => console.log('API Running on port 3000!'));
+app.listen(port, () => console.log(`API Running on port ${port}!`));
