cf7c8befd6af3324e1c41cc3df883fa3f71b0dd0
caiquequaresmasilva <caique.quaresma@gmail.com>
2024-03-02 17:36:04 -0300

    feat(Infra): add domain validation to userRoutes

--- backend/users-api/src/infra/http/routes/userRoutes.ts
+++ backend/users-api/src/infra/http/routes/userRoutes.ts
@@ -1,10 +1,15 @@
 import { Router } from 'express';
 import { makeUserController } from '../../factories';
+import { validationHandler } from '../middlewares';
 
 const userRoutes = Router();
 const userController = makeUserController();
 
-userRoutes.post('/', (req, res) => userController.create(req, res));
+userRoutes.post(
+  '/',
+  (req, res, next) => validationHandler(req, res, next),
+  (req, res) => userController.create(req, res),
+);
 userRoutes.post('/login', (req, res) => userController.signIn(req, res));
 
 export default userRoutes;
