874b954cfe82fbbaa1b22f02ab175ac4b0758267
caiquequaresmasilva <caique.quaresma@gmail.com>
2024-03-02 14:47:51 -0300

    fix(Infra): fix makeUserController

--- backend/users-api/src/infra/factories/makeUserController.ts
+++ backend/users-api/src/infra/factories/makeUserController.ts
@@ -1,6 +1,6 @@
 import { UserService } from '../../application/services';
 import { JwtAdapter } from '../adapters';
-import { UserController } from '../http/controllers/UserController';
+import { UserController } from '../http/controllers';
 import { SequelizeUserRepository } from '../repositories';
 
 export function makeUserController(): UserController {
