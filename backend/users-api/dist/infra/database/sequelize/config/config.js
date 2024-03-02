395df17cbce30b895e48c7d0c8f3140a644171f4
caiquequaresmasilva <caique.quaresma@gmail.com>
2024-03-02 15:19:12 -0300

    fix: update build

--- backend/users-api/dist/infra/database/sequelize/config/config.js
+++ backend/users-api/dist/infra/database/sequelize/config/config.js
@@ -1,6 +1,10 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 require("dotenv/config");
+const pg_1 = __importDefault(require("pg"));
 const config = {
     username: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
@@ -14,6 +18,7 @@ const config = {
             require: true,
         },
     },
+    dialectModule: pg_1.default,
     logging: false,
 };
 module.exports = config;
