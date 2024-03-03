7fb03c31cd7799ae4d0735b4041fb9924cb9ef91
caiquequaresmasilva <caique.quaresma@gmail.com>
2024-03-03 13:21:52 -0300

    fix(Application): fix ProductRepository

--- backend/products-api/src/application/repositories/ProductRepository.ts
+++ backend/products-api/src/application/repositories/ProductRepository.ts
@@ -1,6 +1,6 @@
 import { Product, ProductProps } from '../../domain';
 
-interface FilterParams {
+export interface FilterParams {
   brand?: string;
   color?: string;
   price?: [number, number];
@@ -8,9 +8,11 @@ interface FilterParams {
 
 export abstract class IProductRepository {
   abstract create(data: Product): Promise<void>;
-  abstract update(id: string, data: Product): Promise<void>;
+  abstract update(data: Product): Promise<void>;
   abstract delete(id: string): Promise<void>;
+  abstract findById(id: string): Promise<Product | null>;
   abstract searchByName(name: string): Promise<ProductProps[]>;
   abstract filter(params: FilterParams): Promise<ProductProps[]>;
   abstract findAll(): Promise<ProductProps[]>;
+  abstract exists(data: ProductProps): Promise<boolean>;
 }
