abc88beff9f8ff9255c96bb4d6185db9ff548fe8
caiquequaresmasilva <caique.quaresma@gmail.com>
2024-03-06 13:04:52 -0300

    refact: add select to AddProduct page

--- frontend/cell-phones-app/src/pages/AddProduct/components/AddFormContainer.tsx
+++ frontend/cell-phones-app/src/pages/AddProduct/components/AddFormContainer.tsx
@@ -1,9 +1,19 @@
 import { EditForm } from "../../../components";
+import { useGetOptions } from "../../../hooks";
 
 export default function AddFormContainer() {
+  const { brand, color, loading } = useGetOptions()
   return (
     <div>
-      <EditForm action="CREATE" message="Product created" />
+      {loading ? <p>Loading...</p> : (
+        <EditForm
+          action="CREATE"
+          message="Product created"
+          brands={brand}
+          colors={color}
+        />
+      )}
+
     </div>
   )
 }
