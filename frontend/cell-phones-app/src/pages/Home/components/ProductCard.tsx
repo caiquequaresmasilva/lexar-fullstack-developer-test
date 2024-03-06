dc3203d3c92c48faaae35724beba3fe9f6416525
caiquequaresmasilva <caique.quaresma@gmail.com>
2024-03-06 01:40:58 -0300

    feat: implements ProductCard component base structure

--- frontend/cell-phones-app/src/pages/Home/components/ProductCard.tsx
+++ frontend/cell-phones-app/src/pages/Home/components/ProductCard.tsx
@@ -1,5 +1,33 @@
-export default function ProductCard() {
+import { Link } from "react-router-dom"
+
+interface ProductProps {
+  id: string
+  name: string
+  brand: string
+  model: string
+  price: number
+  color: string
+
+}
+
+export default function ProductCard({ id, name, brand, model, color, price }: ProductProps) {
+
   return (
-    <div>ProductCard</div>
+    <div>
+      <h3>{name}</h3>
+      <div>
+        <ul>
+          <li>Brand: {brand}</li>
+          <li>Model: {model}</li>
+          <li>Color: {color}</li>
+          <li>Price: {price}</li>
+        </ul>
+      </div>
+      <div>
+        <Link to={`/home/edit-product/${id}`}>EDIT</Link>
+        <button>ADD VARIANT</button>
+        <button>DELETE</button>
+      </div>
+    </div>
   )
 }
