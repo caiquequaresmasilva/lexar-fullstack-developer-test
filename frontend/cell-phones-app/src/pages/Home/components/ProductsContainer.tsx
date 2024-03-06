053ccb187c786e337e1be064c26b7940910702d0
caiquequaresmasilva <caique.quaresma@gmail.com>
2024-03-06 01:41:21 -0300

    feat: implements ProductContainer base structure

--- frontend/cell-phones-app/src/pages/Home/components/ProductsContainer.tsx
+++ frontend/cell-phones-app/src/pages/Home/components/ProductsContainer.tsx
@@ -1,5 +1,32 @@
+import { useFetch } from "../../../hooks"
+import { makeFilterUrl } from "../../../utils"
+import { ProductCard } from "."
+
+type Product = {
+  id: string
+  name: string
+  brand: string
+  model: string
+  color: string
+  price: number
+}
 export default function ProductsContainer() {
+  const name = ''
+  const brand = ''
+  const color = ''
+  const minPrice = ''
+  const maxPrice = ''
+
+  const { data, loading } = useFetch<Product[]>(makeFilterUrl({ name, brand, color, minPrice, maxPrice }), [])
+
   return (
-    <div>ProductsContainer</div>
+    <div>
+      {loading ? <p>Loading...</p> : (
+        <>
+          {data.map(prod => <ProductCard {...prod} key={prod.id} />)}
+        </>
+      )}
+
+    </div>
   )
 }
