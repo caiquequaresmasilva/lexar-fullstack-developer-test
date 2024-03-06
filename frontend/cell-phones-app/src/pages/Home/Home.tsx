dc01ed99ec2415381139ba999d590237f75ea6ba
caiquequaresmasilva <caique.quaresma@gmail.com>
2024-03-06 01:41:41 -0300

    feat: add base components to Home page

--- frontend/cell-phones-app/src/pages/Home/Home.tsx
+++ frontend/cell-phones-app/src/pages/Home/Home.tsx
@@ -1,14 +1,16 @@
-import { Link } from "react-router-dom"
+import { FilterTool, ProductsContainer, SearchTool } from "./components"
 
 function Home() {
 
   return (
     <main className="w-full h-screen text-[#FFFFFFD6]">
-      <h1>Home Page</h1>
-      <Link to='/home/edit-product/42'>Edit Product</Link>
       <div>
-        <button>SEARCH</button>
-        <button>FILTER</button>
+        <div>
+          <SearchTool />
+          <FilterTool />
+        </div>
+
+        <ProductsContainer />
       </div>
     </main>
   )
