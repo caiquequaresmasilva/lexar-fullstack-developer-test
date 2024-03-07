a7bf61ceda826704211f51be19f97dd60ec74883
caiquequaresmasilva <caique.quaresma@gmail.com>
2024-03-07 06:20:50 -0300

    feat: change css on Header component

--- frontend/cell-phones-app/src/components/Header.tsx
+++ frontend/cell-phones-app/src/components/Header.tsx
@@ -12,8 +12,8 @@ export default function Header() {
     <>
       <header className="w-full flex justify-between text-zinc-100 p-4">
         <div>
-          <Link className={`mr-4 hover:text-green-600 ${pathname == '/home' && 'text-green-600'}`} to='/home'> HOME</Link>
-          <Link className={`mr-4 hover:text-green-600 ${pathname == '/home/add-product' && 'text-green-600'}`} to='/home/add-product'>ADD PRODUCT</Link>
+          <Link className={`mr-4 hover:text-green-500 ${pathname == '/home' && 'text-green-500'}`} to='/home'> HOME</Link>
+          <Link className={`mr-4 hover:text-green-500 ${pathname == '/home/add-product' && 'text-green-500'}`} to='/home/add-product'>ADD PRODUCT</Link>
         </div>
 
         <div className="bg-green-800 p-3 rounded-full flex w-fit">
