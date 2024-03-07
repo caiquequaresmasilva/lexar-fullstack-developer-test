618b0e54279be4f8fb3c98e97b86a2c02d5fd713
caiquequaresmasilva <caique.quaresma@gmail.com>
2024-03-07 11:44:06 -0300

    feat: add sign out feature

--- frontend/cell-phones-app/src/components/Header.tsx
+++ frontend/cell-phones-app/src/components/Header.tsx
@@ -1,13 +1,18 @@
 import { useEffect, useState } from "react";
-import { Link, Outlet, useLocation } from "react-router-dom";
+import { Link, Outlet, useLocation, useNavigate } from "react-router-dom";
 
 export default function Header() {
   const [user, setUser] = useState('')
   const { pathname } = useLocation()
+  const navigate = useNavigate()
+  const signOut = () => {
+    localStorage.setItem('token', '')
+    navigate('/login')
+  }
 
   useEffect(() => {
     setUser(localStorage.getItem('user') || 'unknown')
-  },[])
+  }, [])
   return (
     <>
       <header className="w-full flex justify-between text-zinc-100 p-4">
@@ -15,10 +20,14 @@ export default function Header() {
           <Link className={`mr-4 hover:text-green-500 ${pathname == '/home' && 'text-green-500'}`} to='/home'> HOME</Link>
           <Link className={`mr-4 hover:text-green-500 ${pathname == '/home/add-product' && 'text-green-500'}`} to='/home/add-product'>ADD PRODUCT</Link>
         </div>
+        <div className="flex">
+          <button onClick={signOut} className="mr-4 hover:text-green-500">SIGN OUT</button>
+          <div className="bg-green-800 p-3 rounded-full flex w-fit">
+            <p>Welcome, {user} !</p>
+          </div>
 
-        <div className="bg-green-800 p-3 rounded-full flex w-fit">
-          <p>Welcome, {user} !</p>
         </div>
+
       </header>
       <Outlet />
     </>
