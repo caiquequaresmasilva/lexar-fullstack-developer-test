e685b6b63f873b6fd823758f379041d101641bb7
caiquequaresmasilva <caique.quaresma@gmail.com>
2024-03-07 07:55:22 -0300

    feat: add css to FilterModal

--- frontend/cell-phones-app/src/pages/Home/components/FilterModal.tsx
+++ frontend/cell-phones-app/src/pages/Home/components/FilterModal.tsx
@@ -13,43 +13,63 @@ export default function FilterModal({ updateParams, open, closeModal, brands = [
   }
   return (
     <div className={`fixed z-50 inset-0 flex justify-center items-center ${open ? "visible bg-black/90" : "invisible"}`}>
-      <div className="bg-slate-950 w-3/4 h-[45%] sm:w-1/4 md:h-1/2 rounded-xl p-4 flex flex-col justify-between min-w-[350px] cursor-default text-black">
-        <form onSubmit={handleForm}>
-          <label htmlFor="filter-brand-select" className="text-white">Brands: </label>
-          <select id='filter-brand-select' name="brand" onChange={({ target }) => setBrand(target.value)}>
-            <option value="">Select a brand</option>
-            {brands?.map(({ id, name }) => (<option key={id}>{name}</option>))}
-          </select>
+      <div className="bg-green-100 w-[35%] h-[40%] rounded-xl p-4 flex flex-col justify-between cursor-default">
+        <form className="flex flex-col justify-between items-center h-[80%] " onSubmit={handleForm}>
 
-          <label htmlFor="filter-color-select" className="text-white">Colors: </label>
-          <select id="filter-color-select" name="color" onChange={({ target }) => setColor(target.value)}>
-            <option value="">Select a color</option>
-            {colors?.map(({ id, name }) => (<option key={id}>{name}</option>))}
-          </select>
+          <div className="flex text-black w-full justify-evenly font-semibold">
+            <label htmlFor="filter-brand-select">
+              Brands:
+              <select className="m-1" id='filter-brand-select' name="brand" onChange={({ target }) => setBrand(target.value)}>
+                <option value="">Select a brand</option>
+                {brands?.map(({ id, name }) => (<option key={id}>{name}</option>))}
+              </select>
+            </label>
 
-          <label htmlFor="filter-minprice-input" className="text-white">Minimum price: </label>
-          <input
-            type="number"
-            id="filter-minprice-input"
-            name="minPrice"
-            value={minPrice}
-            onChange={(e) => setMinPrice(Number(e.target.value))}
-            required
-          />
 
-          <label htmlFor="filter-maxprice-input" className="text-white">Maximum price: </label>
-          <input
-            type="number"
-            id="filter-maxprice-input"
-            name="maxPrice"
-            value={maxPrice}
-            onChange={(e) => setMaxPrice(Number(e.target.value))}
-            required
-          />
+            <label htmlFor="filter-color-select">
+              Colors:
+              <select className="m-1" id="filter-color-select" name="color" onChange={({ target }) => setColor(target.value)}>
+                <option value="">Select a color</option>
+                {colors?.map(({ id, name }) => (<option key={id}>{name}</option>))}
+              </select>
+            </label>
+          </div>
 
-          <button className="text-green-500" type="submit">SEARCH</button>
+          <div className="flex text-black w-full justify-center font-semibold">
+            <label htmlFor="filter-minprice-input" className="mx-4">
+              Min price:
+              <input
+                type="number"
+                className="w-[40%]"
+                id="filter-minprice-input"
+                name="minPrice"
+                value={minPrice}
+                onChange={(e) => setMinPrice(Number(e.target.value))}
+                required
+              />
+            </label>
+
+
+            <label htmlFor="filter-maxprice-input">
+              Max price:
+              <input
+                type="number"
+                className="w-[40%]"
+                id="filter-maxprice-input"
+                name="maxPrice"
+                value={maxPrice}
+                onChange={(e) => setMaxPrice(Number(e.target.value))}
+                required
+              />
+            </label>
+          </div>
+
+
+
+
+          <button className="text-green-900 font-bold hover:text-green-500" type="submit">FILTER</button>
         </form>
-        <button className="text-white" onClick={closeModal}>CLOSE</button>
+        <button className="text-black font-bold" onClick={closeModal}>CLOSE</button>
 
       </div>
 
