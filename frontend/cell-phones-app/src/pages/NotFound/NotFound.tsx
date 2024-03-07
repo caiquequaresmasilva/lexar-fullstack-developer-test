a4996e3537745ed654e1d297bd8e07441fc2bb33
caiquequaresmasilva <caique.quaresma@gmail.com>
2024-03-07 11:07:17 -0300

    feat: creates NotFound page

--- frontend/cell-phones-app/src/pages/NotFound/NotFound.tsx
+++ frontend/cell-phones-app/src/pages/NotFound/NotFound.tsx
@@ -3,10 +3,11 @@ import { Link } from "react-router-dom"
 function NotFound() {
 
   return (
-    <>
-      <h1>NotFound Page</h1>
-      <Link to='/home'>Home</Link>
-    </>
+    <main className='w-full h-screen px-8 py-16 flex flex-col justify-center items-center'>
+      <h1 className="text-6xl text-zinc-200 font-bold">Page not found!</h1>
+      <Link className="text-xl mt-2 text-green-500" to='/home'>Home</Link>
+    </main>
+
   )
 }
 
