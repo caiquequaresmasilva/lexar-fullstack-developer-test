9edd8a42ace3996bc9eb22f8711914a2fe43ec82
caiquequaresmasilva <caique.quaresma@gmail.com>
2024-03-07 05:14:03 -0300

    feat: define css of Header

--- frontend/cell-phones-app/src/components/Header.tsx
+++ frontend/cell-phones-app/src/components/Header.tsx
@@ -1,19 +1,24 @@
 import { useEffect, useState } from "react";
-import { Link, Outlet } from "react-router-dom";
+import { Link, Outlet, useLocation } from "react-router-dom";
 
 export default function Header() {
   const [user, setUser] = useState('')
+  const { pathname } = useLocation()
 
   useEffect(() => {
     setUser(localStorage.getItem('user') || 'unknown')
-  })
+  },[])
   return (
     <>
-      <header className="w-full flex justify-between text-[#FFFFFFD6] p-4">
-        <span>LEXART</span>
-        <Link className="mx-1" to='/home/add-product'>ADD PRODUCT</Link>
-        <Link className="mx-1" to='/home'> HOME</Link>
-        <span>{user}</span>
+      <header className="w-full flex justify-between text-zinc-100 p-4">
+        <div>
+          <Link className={`mr-4 hover:text-green-600 ${pathname == '/home' && 'text-green-600'}`} to='/home'> HOME</Link>
+          <Link className={`mr-4 hover:text-green-600 ${pathname == '/home/add-product' && 'text-green-600'}`} to='/home/add-product'>ADD PRODUCT</Link>
+        </div>
+
+        <div className="bg-green-800 p-3 rounded-full flex w-fit">
+          <p>Welcome, {user} !</p>
+        </div>
       </header>
       <Outlet />
     </>
