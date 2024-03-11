6507efd49162ce838be273f2f6339ab1e5af32b4
caiquequaresmasilva <caique.quaresma@gmail.com>
2024-03-11 15:57:29 -0300

    refact: change Home page components with redux logic

--- frontend/cell-phones-app/src/pages/Home/Home.tsx
+++ frontend/cell-phones-app/src/pages/Home/Home.tsx
@@ -1,49 +1,23 @@
-import { useState } from "react"
-import { FilterModal, ProductsContainer, SearchModal } from "./components"
+import { DeleteModal, FilterModal, FiltersBars, ProductsContainer, SearchModal, VariantModal } from "./components"
 import { useGetOptions } from "../../hooks"
 import { Loading } from "../../components"
 
-const INITIAL_PARAMS: FilterParams = {
-  brand: '',
-  color: '',
-  name: '',
-  minPrice: 0,
-  maxPrice: 0
-
-}
 function Home() {
-  const [filter, setFilter] = useState<FilterParams>(INITIAL_PARAMS)
-  const [openSearch, setOpenSearch] = useState(false)
-  const [openFilters, setOpenFilters] = useState(false)
-
   const { color, brand, loading } = useGetOptions()
-
-  const updateParams = (params: Partial<FilterParams>) => {
-    setFilter(prev => ({ ...prev, ...params }))
-  }
-  const handleSearchButton = () => setOpenSearch(true)
-  const handleFilterButton = () => setOpenFilters(true)
-  const closeSearch = () => setOpenSearch(false)
-  const closeFilter = () => setOpenFilters(false)
-
   return (
     <main className='w-full h-screen px-4 flex justify-center items-center'>
       {loading ? <Loading /> : (
         <>
           <div className="flex flex-col w-full h-screen">
-            <div className="self-start pb-2">
-              <button className="text-zinc-100 hover:text-green-500 mr-4" onClick={() => setFilter(INITIAL_PARAMS)}>ALL</button>
-              <button className="text-zinc-100 hover:text-green-500 mr-4" onClick={handleSearchButton}>SEARCH</button>
-              <button className="text-zinc-100 hover:text-green-500 mr-4" onClick={handleFilterButton}>FILTER</button>
-            </div>
-
-            <ProductsContainer {...filter} colors={color} />
+            <FiltersBars />
+            <ProductsContainer colors={color} />
           </div>
-          <SearchModal updateParams={updateParams} open={openSearch} closeModal={closeSearch} />
-          <FilterModal updateParams={updateParams} open={openFilters} closeModal={closeFilter} brands={brand} colors={color} />
-
         </>
       )}
+      <SearchModal />
+      <FilterModal brands={brand} colors={color} />
+      <VariantModal colors={color} />
+      <DeleteModal/>
     </main>
   )
 }
--- frontend/cell-phones-app/src/pages/Home/components/ProductsContainer.tsx
+++ frontend/cell-phones-app/src/pages/Home/components/ProductsContainer.tsx
@@ -3,20 +3,23 @@ import { makeFilterUrl } from "../../../utils"
 import { ProductCard } from "."
 import { useState } from "react"
 import { Loading } from "../../../components"
+import { useAppSelector } from "../../../redux/hooks"
+import { selectFilters } from "../../../redux/filterSlice"
 
+interface ProductsContainerProps {
+  colors: Option[]
+}
+export default function ProductsContainer({ colors }: ProductsContainerProps) {
+  const { brand, color, maxPrice, minPrice, name } = useAppSelector(selectFilters)
+  const { data, loading } = useFetch<Product[]>(makeFilterUrl({ name, brand, color, minPrice, maxPrice }), [])
 
-export default function ProductsContainer({ brand, color, maxPrice, minPrice, name, colors }: FilterParams & { colors: Option[] }) {
-    const [refresh, setRefresh] = useState(false)
-    const { data, loading } = useFetch<Product[]>(makeFilterUrl({ name, brand, color, minPrice, maxPrice }), [], refresh)
-    const refreshFunction = () => setRefresh(prev => !prev)
-
-    return (
-        <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-2 text-center'>
-            {loading ? <Loading /> : (
-                <>
-                    {data.map(prod => <ProductCard {...prod} key={prod.id} refresh={refreshFunction} colors={colors} />)}
-                </>
-            )}
-        </div>
-    )
+  return (
+    <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-2 text-center'>
+      {loading ? <Loading /> : (
+        <>
+          {data.map(prod => <ProductCard {...prod} key={prod.id} colors={colors} />)}
+        </>
+      )}
+    </div>
+  )
 }
