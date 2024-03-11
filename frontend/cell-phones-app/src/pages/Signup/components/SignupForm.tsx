81d4e12559f13a5530c3a4d91858c5c87da00136
caiquequaresmasilva <caique.quaresma@gmail.com>
2024-03-10 23:01:52 -0300

    refact: add new components to SignupForm

--- frontend/cell-phones-app/src/pages/Signup/components/SignupForm.tsx
+++ frontend/cell-phones-app/src/pages/Signup/components/SignupForm.tsx
@@ -1,6 +1,7 @@
 import { useNavigate } from "react-router-dom";
 import { handleForm } from "../../../utils";
-import { useState } from "react";
+import { useUserInputs } from "../../../hooks";
+import { EmailInput, NameInput, PasswordInput } from "../../../components";
 
 type Inputs = {
   name: string;
@@ -9,9 +10,11 @@ type Inputs = {
 };
 
 function SignupForm() {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [
+    { email, name, password },
+    { setEmail, setName, setPassword }
+  ] = useUserInputs()
+
   const navigate = useNavigate();
 
   const handleSubmit = async (e: React.FormEvent) => {
@@ -20,9 +23,6 @@ function SignupForm() {
       { name, email, password },
       "User created."
     );
-    setName("");
-    setEmail("");
-    setPassword("");
     if (flag) {
       navigate("/home");
     }
@@ -30,35 +30,9 @@ function SignupForm() {
   return (
     <form className="flex flex-col w-1/2 h-full justify-evenly" onSubmit={handleSubmit}>
 
-      <input
-        type="text"
-        name="name"
-        placeholder="Name"
-        className="bg-zinc-100 placeholder-zinc-600 focus:border-green-600 focus:outline-none focus:border-2 p-2 rounded"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-        required
-      />
-      <input
-        type="email"
-        name="email"
-        placeholder="Email"
-        className="bg-zinc-100 placeholder-zinc-600 focus:border-green-600 focus:outline-none focus:border-2 p-2 rounded"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-        required
-      />
-      <input
-        type="password"
-        name="password"
-        placeholder="Password"
-        className="bg-zinc-100 placeholder-zinc-600 focus:border-green-600 focus:outline-none focus:border-2 p-2 rounded"
-        pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$"
-        title="The password must contain at least 8 characters, with uppercase letters, lowercase letters and numbers."
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        required
-      />
+      <NameInput state={name} setState={setName} />
+      <EmailInput state={email} setState={setEmail} />
+      <PasswordInput state={password} setState={setPassword} />
 
       <button className='text-white bg-green-700 rounded py-1 hover:bg-green-600' type="submit">Sign up</button>
     </form>
