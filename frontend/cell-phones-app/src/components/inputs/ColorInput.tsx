f456a8e13c3fc35b0b3899fd8aae5a92e805403b
caiquequaresmasilva <caique.quaresma@gmail.com>
2024-03-11 15:49:23 -0300

    refact: implements ColorInput component

---
+++ frontend/cell-phones-app/src/components/inputs/ColorInput.tsx
@@ -0,0 +1,18 @@
+import { memo } from "react"
+
+type ColorInputsProps = {
+  colors: Option[]
+} & TextInputProps<string>
+
+export default memo(function ColorInput({ colors, state, setState }: ColorInputsProps) {
+  const handleOnChange = ({ target: { value } }: OnChangeSelectType) => setState(value)
+  return (
+    <label htmlFor="variant-color-select" className="font-semibold">
+      Color:
+      <select id="variant-color-select" name="color" onChange={handleOnChange} value={state} className="text-black m-2">
+        <option value="">Select a color</option>
+        {colors.map(({ id, name }) => (<option key={id}>{name}</option>))}
+      </select>
+    </label>
+  )
+})
