80bd0dcf87da5a98267d44465050bf3ee98cc6cb
caiquequaresmasilva <caique.quaresma@gmail.com>
2024-03-11 15:51:48 -0300

    refact: change folder name

---
+++ frontend/cell-phones-app/src/components/inputs/index.ts
@@ -0,0 +1,8 @@
+export { default as BrandInput } from './BrandInput';
+export { default as ColorInput } from './ColorInput';
+export { default as EmailInput } from './EmailInput';
+export { default as MaxPriceInput } from './MaxPriceInput';
+export { default as MinPriceInput } from './MinPriceInput';
+export { default as NameInput } from './NameInput';
+export { default as PasswordInput } from './PasswordInput';
+export { default as PriceInput } from './PriceInput';
--- frontend/cell-phones-app/src/components/textInputs/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as EmailInput } from './EmailInput';
-export { default as NameInput } from './NameInput';
-export { default as PasswordInput } from './PasswordInput';
