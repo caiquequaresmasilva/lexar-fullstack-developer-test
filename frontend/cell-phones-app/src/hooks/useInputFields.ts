6ccdf6e13772bb172aa112729a57f05c9c1a4fad
caiquequaresmasilva <caique.quaresma@gmail.com>
2024-03-11 18:17:19 -0300

    refact: add return type to useInputFields hook

--- frontend/cell-phones-app/src/hooks/useInputFields.ts
+++ frontend/cell-phones-app/src/hooks/useInputFields.ts
@@ -1,11 +1,29 @@
 import { useState } from "react";
 
-export default function useInputFields(inputs?: InputFields) {
+type Return = [
+  {
+    name: string;
+    brand: string;
+    color: string;
+    model: string;
+    price: string;
+  },
+  {
+    setName: SetState<string>;
+    setBrand: SetState<string>;
+    setModel: SetState<string>;
+    setColor: SetState<string>;
+    setPrice: SetState<string>;
+  }
+];
+export default function useInputFields(inputs?: InputFields): Return {
   const [name, setName] = useState(inputs?.name || "");
   const [brand, setBrand] = useState(inputs?.brand || "");
   const [color, setColor] = useState(inputs?.color || "");
   const [model, setModel] = useState(inputs?.model || "");
-  const [price, setPrice] = useState(inputs?.price || 100);
+  const [price, setPrice] = useState(
+    inputs?.price ? inputs?.price.toString() : "0"
+  );
   return [
     { name, brand, color, model, price },
     { setName, setBrand, setModel, setColor, setPrice },
