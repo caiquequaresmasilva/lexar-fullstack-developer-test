018cc48dff423466da379105cfb4d85b2cb9d902
caiquequaresmasilva <caique.quaresma@gmail.com>
2024-03-11 15:56:35 -0300

    feat: Implements VariantModal

---
+++ frontend/cell-phones-app/src/pages/Home/components/modals/VariantModal.tsx
@@ -0,0 +1,69 @@
+import { useState } from "react"
+import { useAppDispatch, useAppSelector } from "../../../../redux/hooks"
+import { selectOpenVariant, toggleVariantModal } from "../../../../redux/modalSlice"
+import { handleProductForm } from "../../../../utils"
+import { ColorInput, PriceInput } from "../../../../components"
+
+interface VariantProps {
+  colors: Option[]
+}
+export default function VariantModal({ colors }: VariantProps) {
+  const { openVariant, variantData } = useAppSelector(selectOpenVariant)
+  const dispatch = useAppDispatch()
+  const { name, brand, color, model, price } = variantData
+
+  const [vcolor, setVcolor] = useState(color)
+  const [vprice, setVprice] = useState(price)
+
+  const handleCancel = () => {
+    dispatch(toggleVariantModal())
+    reset()
+  }
+  const reset = () => {
+    setVcolor('')
+    setVprice(100)
+  }
+
+  const handleForm = async (e: React.FormEvent) => {
+    e.preventDefault()
+    const token = localStorage.getItem('token') || ''
+    const flag = await handleProductForm({
+      product: {
+        name,
+        details: {
+          brand,
+          model,
+          color: vcolor
+        },
+        price: vprice
+      },
+      message: 'Product variant created',
+      token
+    })
+    flag && reset()
+  }
+  return (
+    <div className={`fixed z-50 inset-0 flex justify-center items-center ${openVariant ? "visible bg-black/90" : "invisible"}`}>
+      <div
+        className="bg-green-100 w-3/4 h-[45%] sm:w-1/4 md:h-1/2 rounded-xl p-4 flex flex-col justify-between min-w-[350px] cursor-default"
+      >
+        <div className="flex flex-col justify-between h-1/2 text-lg">
+          <h1 className="text-xl font-bold self-center">Create a variant model</h1>
+          <h2>{name}</h2>
+          <p>Brand: {brand}</p>
+          <p>Model: {model}</p>
+        </div>
+        <form className="flex flex-col justify-center" onSubmit={handleForm}>
+
+          <ColorInput colors={colors} state={vcolor} setState={setVcolor} />
+
+          <PriceInput state={vprice} setState={setVprice} />
+
+          <button className="text-green-900 font-bold hover:text-green-500" type="submit">CREATE</button>
+        </form>
+        <button onClick={handleCancel} className="text-red-900 font-bold hover:text-red-500">CANCEL</button>
+
+      </div>
+    </div>
+  )
+}
