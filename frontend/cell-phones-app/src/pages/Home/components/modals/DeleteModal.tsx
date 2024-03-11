990c6604905d4699d3ea8b8467127366e137e0ae
caiquequaresmasilva <caique.quaresma@gmail.com>
2024-03-11 15:55:13 -0300

    feat: Implements DeleteModal

---
+++ frontend/cell-phones-app/src/pages/Home/components/modals/DeleteModal.tsx
@@ -0,0 +1,31 @@
+import { useDispatch } from "react-redux"
+import { useAppSelector } from "../../../../redux/hooks"
+import { selectOpenDelete, toggleDeleteModal } from "../../../../redux/modalSlice"
+import { handleDeleteProduct } from "../../../../utils"
+
+export default function DeleteModal() {
+  const { deleteId, openDelete } = useAppSelector(selectOpenDelete)
+  const dispatch = useDispatch()
+
+  const handleDelete = async () => {
+    await handleDeleteProduct(deleteId)
+    handleCancel()
+  }
+
+  const handleCancel = () => dispatch(toggleDeleteModal())
+  return (
+    <div className={`fixed z-50 inset-0 flex justify-center items-center ${openDelete ? "visible bg-black/90" : "invisible"}`}>
+      <div
+        className="bg-green-100 w-3/4 h-[45%] sm:w-1/4 md:h-1/2 rounded-xl p-4 flex flex-col justify-between min-w-[350px] cursor-default items-center"
+      >
+        <h1 className="text-xl font-bold">Deleting product</h1>
+
+        <p className="text-lg">Are you sure you want to delete this product? This action cannot be undone.</p>
+
+        <button onClick={handleCancel} className="text-green-900 font-bold hover:text-green-500">CANCEL</button>
+        <button onClick={handleDelete} className="text-red-900 font-bold hover:text-red-500">DELETE</button>
+
+      </div>
+    </div>
+  )
+}
