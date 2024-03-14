76af36423cc0f75544c4466e6e0cb4f76362e417
caiquequaresmasilva <caique.quaresma@gmail.com>
2024-03-14 11:13:55 -0300

    feat: implements regex patterns enums

--- frontend/cell-phones-app/src/utils/patterns.ts
+++ frontend/cell-phones-app/src/utils/patterns.ts
@@ -1,15 +1,24 @@
-export enum UserRegex{
+export enum UserRegex {
   Pattern = "^(?=.*[a-zA-Z])[a-zA-Z\\sáàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ']{3,}$",
-  Message = "The name must contain at least 3 characters, with only letters"
+  Message = "The name must contain at least 3 characters, with only letters",
 }
 
-export enum PasswordRegex{
+export enum PasswordRegex {
   Pattern = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[a-zA-Z\\d]{8,}$",
-  Message = "The password must contain at least 8 characters, with uppercase letters, lowercase letters and numbers."
+  Message = "The password must contain at least 8 characters, with uppercase letters, lowercase letters and numbers.",
 }
 
-export enum ProductRegex{
+export enum ProductRegex {
   Pattern = "^(?=.*[a-zA-Z])[a-zA-Z0-9áàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ'\\s]{3,}$",
-  Message = "The product name must contain at least 3 characters, with only letters and numbers"
+  Message = "The product name must contain at least 3 characters, with only letters and numbers",
 }
 
+export enum ModelRegex {
+  Pattern = "^(?=.*[a-zA-Z])[a-zA-Z0-9áàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ'\\s]{3,}$",
+  Message = "The model must contain at least 3 characters, with only letters and numbers",
+}
+
+export enum EmailRegex {
+  Pattern = "^[a-zA-Z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,10}$",
+  Message = "The email must have the correct format.",
+}
