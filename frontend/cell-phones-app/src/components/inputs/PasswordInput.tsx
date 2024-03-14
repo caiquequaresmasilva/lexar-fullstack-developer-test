52e91c916ed4ceeb28f1f38148bd145c38d2cc8a
caiquequaresmasilva <caique.quaresma@gmail.com>
2024-03-14 11:20:58 -0300

    refact: change PasswordInput with debounce validation

--- frontend/cell-phones-app/src/components/inputs/PasswordInput.tsx
+++ frontend/cell-phones-app/src/components/inputs/PasswordInput.tsx
@@ -1,22 +1,27 @@
-import { memo } from "react"
-import { PasswordRegex } from "../../utils"
+import { memo, useState } from "react"
+import { PasswordRegex, debounceOnChange } from "../../utils"
 
-type PasswordInputProps = {
-  regex: typeof PasswordRegex
-} & TextInputProps<string>
-export default memo(function PasswordInput({ setState, state, regex }: PasswordInputProps) {
-  const handleOnChange = ({ target: { value } }: OnChangeType) => setState(value)
+export default memo(function PasswordInput({ setState }: TextInputProps<string>) {
+  const [error, setError] = useState('')
+  const handleOnChange = ({ target: { value } }: OnChangeType) => {
+    if (RegExp(PasswordRegex.Pattern).test(value)) {
+      setState(value)
+      setError('')
+    } else {
+      setError(PasswordRegex.Message)
+    }
+  }
   return (
-    <input
-      type="password"
-      name="password"
-      placeholder='Password'
-      className="bg-zinc-100 placeholder-zinc-600 focus:border-green-600 focus:outline-none focus:border-2 p-2 rounded"
-      pattern={regex.Pattern}
-      title={regex.Message}
-      value={state}
-      onChange={handleOnChange}
-      required
-    />
+    <>
+      <input
+        type="password"
+        name="password"
+        placeholder='Password'
+        className="bg-zinc-100 placeholder-zinc-600 focus:border-green-600 focus:outline-none focus:border-2 p-2 rounded"
+        onChange={debounceOnChange(handleOnChange, 1000)}
+        required
+      />
+      <p className="text-red-500 text-sm">{error}</p>
+    </>
   )
 })
