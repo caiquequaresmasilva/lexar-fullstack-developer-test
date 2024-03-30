af70d64c88d14ac4b6d70e1e0e6dc7a83c6d7895
caiquequaresmasilva <caique.quaresma@gmail.com>
2024-03-30 13:00:53 -0300

    chore: remove sequelize ORM

--- backend/products-api/src/infra/repositories/index.ts
+++ backend/products-api/src/infra/repositories/index.ts
@@ -1,6 +1,3 @@
 export { default as PrismaBrandRepository } from './PrismaBrandRepository';
 export { default as PrismaColorRepository } from './PrismaColorRepository';
 export { default as PrismaProductRepository } from './PrismaProductRepository';
-export * from './SequelizeBrandRepository';
-export * from './SequelizeColorRepository';
-export * from './SequelizeProductRepository';
